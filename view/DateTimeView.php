d2e1a82055c48116f3a69e8c6e2591c45a5da8ab
Lone Nilsson <onlylonely1986@gmail.com>
2019-09-12 13:29:25 +0200

    fixing datetimeview

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -2,10 +2,18 @@
 
 class DateTimeView {
 
-
+// Thursday, the 12th of September 2019, The time is 11:51:21
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		date_default_timezone_set("Europe/Stockholm");
+		$day = Date("l");
+		$date = Date("d");
+		$month = Date("M");
+		$year = Date("20y");
+		$time = Date("H:i:s");
+		// obs hardcoded "tember" and "th"
+		$timeString = $day . ", the " . $date . "th of " . $month . "tember " . $year . ", The time is " . $time;
+		
 
 		return '<p>' . $timeString . '</p>';
 	}
