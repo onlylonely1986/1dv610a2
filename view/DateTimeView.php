be03f747c125b6fd5ee7523a6e62ca176ee3af20
Lone Nilsson <onlylonely1986@gmail.com>
2019-09-13 08:32:41 +0200

    function for monthName

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -8,11 +8,20 @@ class DateTimeView {
 		date_default_timezone_set("Europe/Stockholm");
 		$day = Date("l");
 		$date = Date("d");
-		$month = Date("M");
+
+		function monthName() {
+			// numeric presentation of month
+			$monthNum = Date("m");
+			// go to string
+			$monthObj = DateTime::createFromFormat('!m', $monthNum);
+			$monthName = $monthObj->format('F');
+			return $monthName;
+		}
+		
 		$year = Date("20y");
 		$time = Date("H:i:s");
 		// obs hardcoded "tember" and "th"
-		$timeString = $day . ", the " . $date . "th of " . $month . "tember " . $year . ", The time is " . $time;
+		$timeString = $day . ", the " . $date . "th of " . monthName() . " " . $year . ", The time is " . $time;
 		
 
 		return '<p>' . $timeString . '</p>';
